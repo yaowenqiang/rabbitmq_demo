4c48fb617f561f0d1c1cf6f1db289b235f857447
yaowenqiang <yaowenqiang111@163.com>
2023-07-25 23:55:05 +0800

    add message  confirmation demo

--- iolooping_asynchronous_publisher.py
+++ iolooping_asynchronous_publisher.py
@@ -11,6 +11,9 @@ class publish_engine:
         self._number_of_messages = 10
         self._channel = None
         self._connection = None
+        self._message_interval = 1
+        self._delivered_messages = 0
+        self.published_messages = 0
 
     def on_open(self, connection):
         print('Reached connection open \n')
@@ -18,22 +21,61 @@ class publish_engine:
 
     def on_declare(self, channel):
         print('Now is on declare')
+        self._channel.confirm_delivery(self.on_delivery_conformation)
+        self.schedule_next_message()
 
-        while self._number_of_messages > 0:
-            print(self._number_of_messages)
-            self._channel.basic_publish(exchange='', routing_key='orders_g',
-                                         body='H' + str(self._number_of_messages),
-                                         properties=pika.BasicProperties(content_type='text/plain', delivery_mode=2)
-                                         )
 
-            self._number_of_messages -= 1
+        # while self._number_of_messages > 0:
+            # print(self._number_of_messages)
+            # self._channel.basic_publish(exchange='', routing_key='orders_g',
+            #                              body='H' + str(self._number_of_messages),
+            #                              properties=pika.BasicProperties(content_type='text/plain', delivery_mode=2)
+            #                              )
 
-        self._connection.close()
+        #     self._number_of_messages -= 1
+        #
+        # self._connection.close()
 
     def on_channel_open(self, channel):
         print('Reached channel open \n')
         argument_list = {'x-queue-master-location': 'random'}
         self._channel.queue_declare('orders_g', callback=self.on_declare, durable=True, arguments=argument_list)
+
+
+    def on_delivery_conformation(self, method_frame):
+        confirmation_type = method_frame.NAME.split('.')[1].lower()
+        self._delivered_messages += 1
+        print('\n'
+              '===================================================================================================='
+              )
+        print(method_frame)
+        print(f'An {confirmation_type} has been received')
+        print(f'Published {self.published_messages} messages and received delivery conformation for {self._delivered_messages} messages')
+        print('\n'
+              '===================================================================================================='
+              )
+
+
+    def publish_message(self):
+        self._channel.basic_publish(exchange='', routing_key='orders_g', body='H' + str(self._number_of_messages),
+                                    properties=pika.BasicProperties(content_type='text/plain', delivery_mode=2)
+                                    )
+
+        self._number_of_messages -= 1
+        self.published_messages += 1
+
+        if self._number_of_messages >= 0:
+            self.schedule_next_message()
+        else:
+            self._connection.close()
+
+
+    def schedule_next_message(self):
+        self._connection.add_timeout(self._message_interval, self.published_messages)
+
+
+
+
 
 
     def on_close(self, reply_code, reply_message):
